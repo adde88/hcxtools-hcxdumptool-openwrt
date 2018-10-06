5e80223cb75afea71fdd43da5b4f6705d4d54f55
Andreas (VM1) <adde88@gmail.com>
2018-10-06 12:31:27 +0200

    Added an installation script that installs both tools

---
+++ INSTALL.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Installation script for hcxdumptool and hcxtools, for the Pineapple NANO and TETRA.
+# Written by: Andreas Nilsen - adde88@gmail.com - https://www.github.com/adde88
+#
+# Starting Install.
+#
+# Variables and colors.
+RED='\033[0;31m'
+NC='\033[0m'
+#
+# Let's always get the latest version
+mkdir -p /tmp/HcxTools
+wget https://github.com/adde88/hcxtools-hcxdumptool-openwrt/tree/master/bin/ar71xx/packages/base -P /tmp/HcxTools
+HCXDUMPTOOL=`grep -F "hcxdumptool_" /tmp/HcxTools/base | awk {'print $5'} | awk -F'"' {'print $2'}`
+HCXTOOLS=`grep -F "hcxtools_" /tmp/HcxTools/base | awk {'print $5'} | awk -F'"' {'print $2'}`
+#
+# Tell the user what's going on...
+echo -e "${RED}Installing: ${NC}hcxdumptool ${RED} and ${NC}hcxtools."
+echo -e "Go grab a cup of coffee, this will take a while...\n"
+#
+# Download latest IPK's to temp directory, and then update OPKG repositories.
+cd /tmp
+opkg update
+wget "https://github.com/adde88/hcxtools-hcxdumptool-openwrt/raw/master/bin/ar71xx/packages/base/"$HCXTOOLS""
+wget "https://github.com/adde88/hcxtools-hcxdumptool-openwrt/raw/master/bin/ar71xx/packages/base/"$HCXDUMPTOOL""
+#
+# Install procedure
+if [ -e /sd ]; then
+	# Nano install (Let's use the SD-card, if it exists)
+	opkg --dest sd install "$HCXTOOLS" "$HCXDUMPTOOL"
+else
+	# Tetra install / general install.
+	opkg install "$HCXTOOLS" "$HCXDUMPTOOL"
+fi
+#
+# Cleanup
+rm -f "$HCXTOOLS" "$HCXDUMPTOOL" /tmp/HcxTools
+echo -e "${RED}Installation completed!"
+exit 0
